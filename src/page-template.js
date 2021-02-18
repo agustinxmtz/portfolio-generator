fa602973e964746f29c6a43e36c3d0f865745b9f
agustinxmtz <70080354+agustinxmtz@users.noreply.github.com>
2021-02-17 18:59:23 -0600

    Created the About section in the HTMl

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,16 @@
+// create the about section
+const generateAbout = aboutText => {
+    if (!aboutText) {
+      return '';
+    }
+  
+    return `
+      <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+      </section>
+    `;
+  };
 module.exports = templateData => {
     // destructure page data by section
     const { projects, about, ...header } = templateData;
